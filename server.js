383e58d8af66f1ab6503814d547d48677e6e287d
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-09-25 15:42:36 +0500

    sdsd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 5500;
+const PORT = 4000;
 
 // Statik fayllar (html, css, js, rasm)
 app.use(express.static(path.join(__dirname)));
